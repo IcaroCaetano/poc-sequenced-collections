0bff76b6a2c761f9e50cb811438e62701c81b7d9
ICAROCAETANO <icaro.caetano@gmail.com>
2026-07-13 22:43:32 -0300

    first commit

---
+++ src/main/java/com/project/poc_sequenced_collections/PocSequencedCollectionsApplication.java
@@ -0,0 +1,13 @@
+package com.project.poc_sequenced_collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocSequencedCollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocSequencedCollectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/poc_sequenced_collections/PocSequencedCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.poc_sequenced_collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocSequencedCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
